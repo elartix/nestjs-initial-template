b057ffaa53486ba3359a88450160f8afe2a90b63
Volodymyr Mechkauskas <vmechkauskas@gmail.com>
2025-08-09 04:17:22 +0300

    - fix(test) fixed app.e2e-spec.ts

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,10 +1,11 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
-import * as request from 'supertest';
+import request from 'supertest';
 import { App } from 'supertest/types';
 import { AppModule } from '../src/app.module';
 
 describe('AppController (e2e)', () => {
+  // let app: INestApplication;
   let app: INestApplication<App>;
 
   beforeEach(async () => {
@@ -16,10 +17,8 @@ describe('AppController (e2e)', () => {
     await app.init();
   });
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
+  it('/ (GET)', async () => {
+    const server = app.getHttpServer() as unknown as import('http').Server;
+    await request(server).get('/').expect(200).expect('Hello World!');
   });
 });
